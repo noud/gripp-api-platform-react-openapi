183a7c76444192f0bf8fa0d648159f84ca71ddd2
Noud de Brouwer <noud4@home.nl>
2020-08-17 11:48:35 +0200

    bin

--- bin/provision.sh
+++ bin/provision.sh
@@ -3,14 +3,15 @@ APP_NAME=gripp-client
 
 rm -r ${APP_NAME} node_modules package-lock.json yarn.lock
 
-yarn install
+yarn install; yarn upgrade
 node_modules/.bin/create-react-app ${APP_NAME}
 
 cd ${APP_NAME}
-# yarn add --dev @api-platform/client-generator
-yarn add --dev https://github.com/api-platform/client-generator#master
 yarn add redux react-redux redux-thunk redux-form react-router-dom connected-react-router prop-types lodash
 yarn add bootstrap font-awesome
+
+# yarn add --dev @api-platform/client-generator
+yarn add --dev https://github.com/api-platform/client-generator#master
 
 cd .. && bin/generate.sh
 cd ${APP_NAME}
